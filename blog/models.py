eca52fb0e1322ff5d3a7de3f6bc91fa9cceec1ce
Sharath Chandra <scsharath2@gmail.com>
2018-10-25 12:28:53 +0530

    Model Code
    Code Added in Blog directory, file Model.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_lenght=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default.timezone)
+    author = models.Foreignkey(User,onDelete=models.CASCADE)
