bd408fbf3d5f3d49d0f379d4b3e4fa9173dfd508
Sharath Chandra <scsharath2@gmail.com>
2018-10-31 13:07:08 +0530

    Database and Migrations

--- blog/models.py
+++ blog/models.py
@@ -9,5 +9,5 @@ class Post(models.Model):
     #author = models.Foreignkey(User,on_delete=models.CASCADE)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
